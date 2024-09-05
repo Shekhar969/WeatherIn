b0b2af95d7bd30e1c89313e988d6ca87bf643888
Shekhar969 <shekharrawal96@gmail.com>
2024-09-05 19:55:44 +0545

    Jssss

--- Script.js
+++ Script.js
@@ -1,8 +1,16 @@
 const UserInput = document.getElementById("userInput")
 const Btn =document.getElementById("Submit")
+const inFo =document.getElementById("info")
+const apiKey="0d94baf1cb9fb0f9165274c3f5c42ee2";
 
-let Api =""
 
-Btn.addEventListener("click",()=>{
-    
+Btn.addEventListener("click" , ()=>{
+    UserInputvalue =UserInput.value;
+    // console.log(UserInputvalue)
+
+    fetch="https://api.openweathermap.org/data/2.5/weather?q=${UserInputvalue}&appid=0d94baf1cb9fb0f9165274c3f5c42ee2"
+    .then(Response => Response.json())
+    .then(data =>{
+        inFo.innerHTML=data;
+    })
 })
